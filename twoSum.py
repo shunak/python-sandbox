f0a7a972449a322a6494b070fab73fd87f5a8c5f
shunak <shuna966@gmail.com>
2019-06-30 14:05:54 +0900

    190630

---
+++ twoSum.py
@@ -0,0 +1,16 @@
+class Solution:
+ def twoSum(self, nums, target):
+     seen = {}
+     for i, num in enumerate(nums):
+         remaining = target - num
+         if remaining in seen:
+            return [seen[remaining], i]
+         seen[num] = i
+     return []
+
+
+
+
+
+
+
